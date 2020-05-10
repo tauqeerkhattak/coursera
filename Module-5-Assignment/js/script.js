914004782dc0d7de9ec422bd77b462da26656a07
Tauqeer Ahmed <tauqeer745@outlook.com>
2020-05-10 14:47:03 +0500

    commit

--- Module-5-Assignment/js/script.js
+++ Module-5-Assignment/js/script.js
@@ -116,7 +116,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl,"randomCategoryShortName",chosenCategoryShortName);
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName",chosenCategoryShortName);
       console.log(chosenCategoryShortName);
 
 
